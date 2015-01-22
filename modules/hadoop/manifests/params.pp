44b5d71e54b1f79d82cc7821344d0ed5d652192a
Weijia Li <liweijia_20101@sina.com>
2015-01-22 13:07:40 +0000

    modify params.pp

--- modules/hadoop/manifests/params.pp
+++ modules/hadoop/manifests/params.pp
@@ -1,10 +1,28 @@
 class hadoop::params {
-  
+
+
+  $hadoop_version = $::hostname ? {
+    default => '2.2.0',
+  }
+
   $hadoop_user = $::hostname ? {
-    default => 'ubuntu',
+    default => 'hadoop',
   }
- 
+
   $hadoop_group = $::hostname ? {
-    default => 'ubuntu',
+    default => 'hadoop',
   }
+
+  $hadoop_master = $::hostname ? {
+    default => 'hadoop-master',
+  }
+
+  $hadoop_slave = $::hostname ? {
+    default => ['hadoop-slave1'],
+  }
+
+  $hadoop_base = $::hostname ? {
+    default => "/home/${hadoop::params::hadoop_user}",
+  }
+
 }

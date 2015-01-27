10bc19bf7ce22152ab2dc02740a48843ffa728d5
Weijia Li <liweijia_20101@sina.com>
2015-01-27 12:30:14 +0000

    change hadoop module framework

--- modules/hadoop/manifests/init.pp
+++ modules/hadoop/manifests/init.pp
@@ -1,18 +1,18 @@
-class hadoop {
+class hadoop ($hadoop_version, $hadoop_group, $hadoop_user, $hadoop_base, $hadoop_master, $hadoop_slaves) {
 
-  require hadoop::params
+  include java
 
   #Add Hadoop User and Group
 
-  group { "${hadoop::params::hadoop_group}":
+  group { "$hadoop_group":
     ensure => present,
   }
 
-  user { "${hadoop::params::hadoop_user}":
+  user { "$hadoop_user":
     ensure => present,
-    home => "/home/${hadoop::params::hadoop_user}",
+    home => "/home/$hadoop_user",
     managehome => true,
-    require => Group["${hadoop::params::hadoop_group}"],
+    require => Group["$hadoop_group"],
   }
 
   #Add "sudo" privilege for hadoop_user
@@ -27,89 +27,89 @@ class hadoop {
 
   #Add Nodes To Hosts, Make Local DNS
 
-  file { '/etc/hosts':
-    ensure => present,
-    owner => 'root',
-    group => 'root',
-    mode => 0644,
-    alias => 'etc-hosts',
-    source => 'puppet:///modules/hadoop/hosts/hosts',
-  }
+#  file { '/etc/hosts':
+#    ensure => present,
+#    owner => 'root',
+#    group => 'root',
+#    mode => 0644,
+#    alias => 'etc-hosts',
+#    source => 'puppet:///modules/hadoop/hosts/hosts',
+#  }
 
   #Configure SSH, No Password Login
 
-  file { "/home/${hadoop::params::hadoop_user}/.ssh/":
-    ensure => "directory",
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "/home/$hadoop_user/.ssh/":
+    ensure => directory,
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0700,
-    alias => "${hadoop::params::hadoop_user}-ssh-dir",
+    alias => "$hadoop_user-ssh-dir",
   }
 
-  file { "/home/${hadoop::params::hadoop_user}/.ssh/id_rsa.pub":
+  file { "/home/$hadoop_user/.ssh/id_rsa.pub":
     ensure => present,
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     source => 'puppet:///modules/hadoop/ssh/id_rsa.pub',
-    require => File["${hadoop::params::hadoop_user}-ssh-dir"],
+    require => File["$hadoop_user-ssh-dir"],
   }
 
-  file { "/home/${hadoop::params::hadoop_user}/.ssh/id_rsa":
+  file { "/home/$hadoop_user/.ssh/id_rsa":
     ensure => present,
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0600,
     source => 'puppet:///modules/hadoop/ssh/id_rsa',
-    require => File["${hadoop::params::hadoop_user}-ssh-dir"],
+    require => File["$hadoop_user-ssh-dir"],
   }
 
-  file { "/home/${hadoop::params::hadoop_user}/.ssh/authorized_keys":
+  file { "/home/$hadoop_user/.ssh/authorized_keys":
     ensure => present,
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     source => 'puppet:///modules/hadoop/ssh/id_rsa.pub',
-    require => File["${hadoop::params::hadoop_user}-ssh-dir"],
+    require => File["$hadoop_user-ssh-dir"],
   }
 
   #Untar Hadoop Package
 
-  file {"${hadoop::params::hadoop_base}":
+  file {"$hadoop_base":
     ensure => directory,
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode  => 0755,
     alias => 'hadoop-base',
   }
 
-#  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}.tar.gz":
-#    owner => "${hadoop::params::hadoop_user}",
-#    group => "${hadoop::params::hadoop_group}",
-#    mode => 0664,
-#    source => "puppet:///modules/hadoop/hadoop-${hadoop::params::hadoop_version}.tar.gz",
-#    alias => 'hadoop-source-tgz',
-#    before => Exec['untar-hadoop'],
-#    require => File['hadoop-base'],
-#  }
+  file { "$hadoop_base/hadoop-$hadoop_version.tar.gz":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
+    mode => 0664,
+    source => "/home/ubuntu/hadoop-$hadoop_version.tar.gz",
+    alias => 'hadoop-source-tgz',
+    before => Exec['untar-hadoop'],
+    require => File['hadoop-base'],
+  }
 
-  exec { "untar hadoop-${hadoop::params::hadoop_version}.tar.gz":
-    command => "tar -zxf hadoop-${hadoop::params::hadoop_version}.tar.gz -C ${hadoop::params::hadoop_base}",
-    cwd => '/home/ubuntu',
-    creates => "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}",
+  exec { "untar hadoop-$hadoop_version.tar.gz":
+    command => "tar -zxf hadoop-$hadoop_version.tar.gz",
+    cwd => "$hadoop_base",
+    creates => "$hadoop_base/hadoop-$hadoop_version",
     alias => 'untar-hadoop',
-    onlyif => "test 0 -eq $(ls -al ${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version} | grep -c bin)",
-#    require => File['hadoop-source-tgz'],
-    user => "${hadoop::params::hadoop_user}",
+    onlyif => "test 0 -eq $(ls -al $hadoop_base/hadoop-$hadoop_version | grep -c bin)",
+    require => File['hadoop-source-tgz'],
+    user => "$hadoop_user",
     before => File['hadoop-app-dir'],
     path => ['/bin', '/usr/bin', '/usr/sbin'],
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}":
+  file { "$hadoop_base/hadoop-$hadoop_version":
      ensure => directory,
      mode => 0755,
-     owner => "${hadoop::params::hadoop_user}",
-     group => "${hadoop::params::hadoop_group}",
+     owner => "$hadoop_user",
+     group => "$hadoop_group",
      alias => "hadoop-app-dir",
      require => Exec['untar-hadoop'],
   }
@@ -135,99 +135,99 @@ class hadoop {
 
   #Create Name、Data And Tmp Directory
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/dfs":
+  file { "$hadoop_base/hadoop-$hadoop_version/dfs":
     ensure => directory,
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     alias => 'hadoop-dfs',
     require => File['hadoop-app-dir'],
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/dfs/name":
+  file { "$hadoop_base/hadoop-$hadoop_version/dfs/name":
     ensure => directory,
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     alias => 'hadoop-dfs-name',
     require => File['hadoop-dfs'],
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/dfs/data":
+  file { "$hadoop_base/hadoop-$hadoop_version/dfs/data":
     ensure => directory,
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     alias => 'hadoop-dfs-data',
     require => File['hadoop-dfs'],
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/tmp":
+  file { "$hadoop_base/hadoop-$hadoop_version/tmp":
     ensure => directory,
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     alias => 'hadoop-tmp',
     require => File['hadoop-app-dir'],
   }
 
   #Configure Hadoop
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/etc/hadoop/hadoop-env.sh":
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "$hadoop_base/hadoop-$hadoop_version/etc/hadoop/hadoop-env.sh":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     alias => 'hadoop-env-sh',
     content => template('hadoop/conf/hadoop-env.sh.erb'),
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/etc/hadoop/yarn-env.sh":
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "$hadoop_base/hadoop-$hadoop_version/etc/hadoop/yarn-env.sh":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     alias => 'yarn-env-sh',
     content => template('hadoop/conf/yarn-env.sh.erb'),
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/etc/hadoop/master":
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "$hadoop_base/hadoop-$hadoop_version/etc/hadoop/master":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     alias => 'hadoop-master',
     content => template('hadoop/conf/master.erb'),
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/etc/hadoop/slaves":
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "$hadoop_base/hadoop-$hadoop_version/etc/hadoop/slaves":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     alias => 'hadoop-slave',
     content => template('hadoop/conf/slaves.erb'),
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/etc/hadoop/core-site.xml":
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "$hadoop_base/hadoop-$hadoop_version/etc/hadoop/core-site.xml":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     alias => 'core-site-xml',
     content => template('hadoop/conf/core-site.xml.erb'),
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/etc/hadoop/hdfs-site.xml":
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "$hadoop_base/hadoop-$hadoop_version/etc/hadoop/hdfs-site.xml":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     alias => 'hdfs-site-xml',
     content => template('hadoop/conf/hdfs-site.xml.erb'),
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/etc/hadoop/mapred-site.xml":
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "$hadoop_base/hadoop-$hadoop_version/etc/hadoop/mapred-site.xml":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     alias => 'mapred-site-xml',
     content => template('hadoop/conf/mapred-site.xml.erb'),
   }
 
-  file { "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/etc/hadoop/yarn-site.xml":
-    owner => "${hadoop::params::hadoop_user}",
-    group => "${hadoop::params::hadoop_group}",
+  file { "$hadoop_base/hadoop-$hadoop_version/etc/hadoop/yarn-site.xml":
+    owner => "$hadoop_user",
+    group => "$hadoop_group",
     mode => 0644,
     alias => 'yarn-site-xml',
     content => template('hadoop/conf/yarn-site.xml.erb'),

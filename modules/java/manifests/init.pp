84e34e6f84cb7dc1ae12786c1298355b67575009
Weijia Li <liweijia_20101@sina.com>
2015-01-27 12:33:39 +0000

    change java module framework

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -11,25 +11,25 @@ class java {
     alias => 'java-base',
   }
         
-#  file { "${java::params::java_base}/jdk${java::params::java_version}.tar.gz":
-#    mode => 0644,
-#    owner => 'root',
-#    group => 'root',
-#    source => "puppet:///modules/java/jdk${java::params::java_version}.tar.gz",
-#    alias => 'java-source-tgz',
-#    before => Exec['untar-java'],
-#    require => File['java-base'],
-#  }
+  file { "${java::params::java_base}/jdk${java::params::java_version}.tar.gz":
+    mode => 0664,
+    owner => 'root',
+    group => 'root',
+    source => "/home/ubuntu/jdk${java::params::java_version}.tar.gz",
+    alias => 'java-source-tgz',
+    require => File['java-base'],
+    before => Exec['untar-java'],
+  }
 	
   exec { "untar jdk${java::params::java_version}.tar.gz":
-    command => "tar -zxf jdk${java::params::java_version}.tar.gz -C ${java::params::java_base}",
-    cwd => '/home/ubuntu',
+    command => "tar -zxf jdk${java::params::java_version}.tar.gz",
+    cwd => "${java::params::java_base}",
     path => ['/bin', '/usr/bin', '/usr/sbin'],
     creates => "${java::params::java_base}/jdk${java::params::java_version}",
     alias => 'untar-java',
-    require => File["${java::params::java_base}"],
-#    refreshonly => true,
-#    subscribe => File['java-source-tgz'],
+    require => [ File["${java::params::java_base}"],File['java-source-tgz'] ],
+    refreshonly => true,
+    subscribe => File['java-source-tgz'],
     before => File['java-app-dir'],
   }
 	

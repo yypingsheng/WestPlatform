444502635484b065e51f208f8b28b4ce3c5befb9
yypingsheng <liweijia_20101@sina.com>
2015-04-22 13:17:23 +0000

    start cluster after configuration

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,35 +1,18 @@
-node 'tian-PC' {
-    $site_domain = 'tian-PC.example.com'
-    
-    nginx::website { 'my-pc':
-        site_domain => 'tian-PC.example.com',
-    }
-    
-    exec { 'retrieve home page':
-        command => "wget -o stdout http://$site_domain > /dev/null",
-        path => ['/bin', '/usr/bin'],
-    }
-
-    class { 'ntp':
-        server => 'us.pool.ntp.org',
-    }
-}
-
 #hadoop params
 $h_version = '2.2.0'
 $h_group = 'hadoop'
 $h_user = 'hadoop'
 $h_base = "/home/$h_user"
-$h_master = 'hadoop-master'
-$h_slaves = ['hadoop-slave3']
-$h_hosts = "172.16.0.64 hadoop-master\n172.16.0.48 hadoop-slave1\n172.16.0.56 hadoop-slave2\n172.16.0.65 hadoop-slave3\n"
+$h_master = 'h1'
+$h_slaves = ['h2']
+$h_hosts = "172.16.0.106 h1\n172.16.0.107 h2\n"
 
 #spark params
 $sc_version = '2.10.4'
 $sp_version = '1.0.0-bin-hadoop2'
 $sp_slaves = $h_slaves
 
-node 'hadoop-master' {
+node 'h1' {
   class { 'hadoop':
     hadoop_version => $h_version,
     hadoop_group => $h_group,
@@ -38,6 +21,12 @@ node 'hadoop-master' {
     hadoop_master => $h_master,
     hadoop_slaves => $h_slaves,
     hosts => $h_hosts,    
+  }
+
+  class { 'hadoop::cluster':
+    hadoop_user => $h_user,
+    hadoop_base => $h_base,
+    hadoop_version => $h_version,
   }
 
   class { 'spark':
@@ -50,7 +39,7 @@ node 'hadoop-master' {
   }
 }
 
-node /hadoop-slave\d*/ {
+node /h2/ {
   class { 'hadoop':
     hadoop_version => $h_version,
     hadoop_group => $h_group,
--- modules/hadoop/manifests/cluster.pp
+++ modules/hadoop/manifests/cluster.pp
@@ -1,22 +1,25 @@
-class hadoop::cluster {
+class hadoop::cluster ($hadoop_user, $hadoop_base, $hadoop_version) {
 
-  require hadoop::params
   require hadoop
 
   exec { 'format namenode':
-    command => './hdfs namenode -format',
-    cwd => "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/bin",
-    creates => "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/tmp/dfs/name/current/VERSION",
+    command => './hadoop namenode -format',
+    user => "$hadoop_user",
+    cwd => "$hadoop_base/hadoop-$hadoop_version/bin",
+    creates => "$hadoop_base/hadoop-$hadoop_version/tmp/dfs/name/current/VERSION",
     alias => 'format-hdfs',
-    path => ['/bin', 'sbin', '/usr/bin', '/usr/sbin', "${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/bin"],
+    path => ['/bin', '/usr/bin', "$hadoop_base/hadoop-$hadoop_version/bin"],
     require => File['hadoop-master'],
-#   before => Exec['start-all'],
+    before => Exec['start-all'],
   }
 
-#  exec { 'start all services':
-#    command => './start-all.sh',
-#    cwd => '${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/sbin',
-#    alias => 'start-all',
-#    path => ['/bin', 'sbin', '/usr/bin', '/usr/sbin', '${hadoop::params::hadoop_base}/hadoop-${hadoop::params::hadoop_version}/sbin'],
-#  }
+  exec { 'start all services':
+    command => './start-all.sh',
+    user => "$hadoop_user",
+    cwd => "$hadoop_base/hadoop-$hadoop_version/sbin",
+    alias => 'start-all',
+    path => ['/bin', 'sbin', '/usr/bin', '/usr/sbin', "$hadoop_base/hadoop-$hadoop_version/sbin"],
+    require => Exec['format-hdfs'],
+  }
+
 }
